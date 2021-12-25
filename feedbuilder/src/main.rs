101a7125f8b5c310a7a62e6ae302a0a8edf7456a
Circe <paz@twowaysix.com>
2021-12-25 14:40:59 +0100

    Working feed creation in same sub directory relative to executable in same folder

--- feedbuilder/src/main.rs
+++ feedbuilder/src/main.rs
@@ -1,8 +1,11 @@
 use std::fs;
 use std::fs::File;
+use std::io::prelude::*;
+
+use std::path::PathBuf;
 use actix_web::client::Client;
 use actix_web::dev::JsonBody;
-use actix_web::web::{Json, Payload};
+use actix_web::web::{Json, Path, Payload};
 use serde::{Serialize, Deserialize};
 use serde_json::Value;
 
@@ -16,10 +19,8 @@ async fn main() {
     let feed_template_path = "./templates/feed_template.xml".to_string();
     let host_url = "http://192.168.178.103:8765".to_string();
 
-    let rss_feed = create_rss_feed(&host_url, &feed_template_path);
 
     let mut client = Client::default();
-
     let payload =
         client
             .get(format!("{}/api/mp3", host_url))
@@ -30,29 +31,29 @@ async fn main() {
             .limit(20_000_000)  // sets max allowable payload size
             .await
             .unwrap();
+    let file_list = serde_json::from_slice::<FileList>(&payload).unwrap();
 
     println!("\n\nResponse: {:?}", payload);
+    println!("Response: {:?}", file_list);
 
-    let obj = serde_json::from_slice::<FileList>(&payload).unwrap();
+    let rss_feed = create_rss_feed(&host_url, &feed_template_path, &file_list);
 
-    println!("Response: {:?}", obj);
-
-
-    // TODO: Replace previous feed.xml
+    let path = PathBuf::from("./static/rss/feed.xml");
+    write_feed(&path, &rss_feed);
 }
 
 fn create_enclosed_tag(tag: &str, child: &str) -> String {
     format!("<{}>{}</{}>", tag, child, tag)
 }
 
-fn create_rss_feed(host_url: &str, feed_template_path: &str) -> String {
+fn create_rss_feed(host_url: &str, feed_template_path: &str, file_list: &FileList) -> String {
     let feed_path = format!("{}/rss/feed.xml", host_url);
     let logo_path = format!("{}/images/logo.png", host_url);
 
     let template = fs::read_to_string(feed_template_path)
             .expect("Something went wrong reading the file");
 
-    let items = create_feed_items(&host_url);
+    let items = create_feed_items(&host_url, file_list);
 
     let feed = template
         .replace("{host_url}", &host_url)
@@ -64,26 +65,22 @@ fn create_rss_feed(host_url: &str, feed_template_path: &str) -> String {
     feed
 }
 
-fn create_feed_items(host_url: &str) -> String {
+fn create_feed_items(host_url: &str, file_list: &FileList) -> String {
     let mut items = Vec::new();
-    let mut episode_urls = Vec::new();
 
-    // TODO: Read feed items from mp3 folder
-    let episode_url = format!("{}/mp3/Riesenkomet.mp3", host_url);
-
-    episode_urls.push(episode_url);
-
-    for url in episode_urls {
+    for file_name in &file_list.file_names {
         // TODO: Calculate duration
         let duration = "734.7461224489796".to_string();
-        items.push(create_feed_item(&url, &duration));
+        items.push(create_feed_item(&host_url, &file_name, &duration));
     }
     items.join("\n")
 }
 
-fn create_feed_item(episode_url: &str, duration: &str) -> String {
+fn create_feed_item(host_url: &str, file_name: &str, duration: &str) -> String {
+    let episode_url = format!("{}/mp3/{}", host_url, file_name);
+
     let item = format!(
-        "<item>
+        "        <item>
             <title>
                 {title}
             </title>
@@ -130,10 +127,16 @@ fn create_feed_item(episode_url: &str, duration: &str) -> String {
                 NO
             </itunes:explicit>
         </item>",
-         title="Title",
+         title=file_name,
          episode_url=episode_url,
          image_url=episode_url.replace("mp3", "jpg"),
          duration=duration
     );
     item
+}
+
+fn write_feed(path: &PathBuf, feed: &str) -> std::io::Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(feed.as_bytes())?;
+    Ok(())
 }
